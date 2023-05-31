ff805e297326460ff73bf5e908a398d5e7186168
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-31 05:39:48 +0300

    register models in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Tasks, Type
+
+
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    pass
