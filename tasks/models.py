b622a46bedaa7419e8cefcf64ccfa95fd1e44287
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-31 05:39:35 +0300

    add models type and tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Type(models.Model):
+    title = models.CharField(max_length='48', verbose_name='Название')
+
+    def __str__(self):
+        return self.title
+
+
+class Tasks(models.Model):
+    condition = models.CharField(max_length=2000, verbose_name='Условие')
+    answer = models.IntegerField(verbose_name='Ответ')
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, related_name='tasks', verbose_name='Тип')
+
+    def __str__(self):
+        return self.id
