0a9097cd08a57bca5222ee8a0c21539cbcab8bbd
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-31 05:41:17 +0300

    makemigrations

--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Type(models.Model):
-    title = models.CharField(max_length='48', verbose_name='Название')
+    title = models.CharField(max_length=48, verbose_name='Название')
 
     def __str__(self):
         return self.title
